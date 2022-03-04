907f25d620897edd7a2c03af5474f2027187447b
Nishant Kumar <nishantdinayak11@gmail.com>
2022-03-04 19:29:22 +0530

    second

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,16 @@ import '../styles/App.css';
 
 const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
 const App = () => {
-
+  
+  const list =(props)=>{
+      return <div>{props.searchArray}</div>
+  }
   
   return (
     <div id="main">
       <form>
         <label for="search">Search</label><br />
-        <input type ="text" id="search-input" className="text" /><br />
+        <input type ="text" value ={props.searchArray} onChange={props.editsearchArray} id="search-input" className="text" /><br />
         <label for ="result">Result</label>
 
       </form>
